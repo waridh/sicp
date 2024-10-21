90d580b292ea258065abe5688fb8808698f0119c
waridh <waridh@proton.me>
2024-10-21 18:14:09 -0400

    moved the exercises into org mode file for literate programming

--- exercises/chapter-1/e-1.4.rkt
+++
@@ -1,2 +0,0 @@
-#lang sicp
-;; If b is greater than 0, add a and b, else subtract a and b
--- exercises/chapter-1/e-1.6.rkt
+++
@@ -1,3 +0,0 @@
-#lang sicp
-;; Using the reimplementation of the if special method will cause the operator
-;; to be evaluated, increasing total evaluations to increase by around 25%
