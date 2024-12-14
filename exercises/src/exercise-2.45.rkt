81d4694eb7ddcb6282da3e8359b8b3dfcd5bf813
waridh <90401236+waridh@users.noreply.github.com>
2024-12-14 16:15:23 -0500

    added the implementation for exercise 2.45

---
+++ exercises/src/exercise-2.45.rkt
@@ -0,0 +1,15 @@
+;; [[file:../chapter-2.org::*Solution][Solution:1]]
+#lang sicp
+(#%require sicp-pict)
+(define (split first-proc second-proc)
+  (define (proc p n)
+    (if (= n 0)
+        p
+        (let ([rec (proc p (- n 1))]) (first-proc p (second-proc rec rec)))))
+  proc)
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(paint (right-split einstein 10))
+(paint (up-split einstein 10))
+;; Solution:1 ends here
