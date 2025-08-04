a2b6ee2cac1d9844c14abf87b9d93f901e24d5c6
waridh <waridh.git@pm.me>
2025-08-03 21:08:55 -0400

    updated the assertions on the 3.73 exercise

--- exercises/chapter-3/src/exercise-73.rkt
+++ exercises/chapter-3/src/exercise-73.rkt
@@ -3,7 +3,8 @@
 (#%require "modules/stream-base.rkt"
            "modules/stream-combinator.rkt"
            "modules/stream-generator.rkt"
-           "modules/stream-signal.rkt")
+           "modules/stream-signal.rkt"
+           "modules/assert-tool.rkt")
 ;; Solution:1 ends here
 
 ;; [[file:../exercise-73.org::*Solution][Solution:2]]
@@ -24,3 +25,10 @@
 (newline)
 (display-stream-range 0 100 (RC1 ones 0))
 ;; Solution:3 ends here
+
+;; [[file:../exercise-73.org::*Solution][Solution:4]]
+(define target-test-stream (RC1 ones 0))
+(assert (= (stream-ref target-test-stream 0) 5) "expecting 5")
+(assert (= (stream-ref target-test-stream 7) 8.5) "expecting 8.5")
+(assert (= (stream-ref (RC1 integers 1) 2) 17.5) "expecting (stream-ref (RC1 integers 1) 2) to be 17.5")
+;; Solution:4 ends here
